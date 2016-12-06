bf3acc9b3dfce5a252ca2a2c8694d8bf916a457e
anmolgupta23 <anmolgupta@college.harvard.edu>
2016-12-06 16:26:36 -0500

    Anmol Changed his files

--- src/features/unique_words.py
+++ src/features/unique_words.py
@@ -4,25 +4,21 @@ from collections import Counter
 #COUNTING THE NUMBER OF UNIQUE WORDS#
 #####################################
 
-def fill_unique_words_column(train_df, valid_df):
+def fill_unique_words_column(train_df, valid_df, train_essays, valid_essays):
 
     #percentage of unique words to the total number of words
     unique_word_percentages_train = []
     unique_word_percentages_valid = []
 
-    for i in range(len(train_df)):
-        splits = train_df.iloc[i]["essay"].split()
-        total_words = len(splits)
+    for i in range(len(train_essays)):
+        splits = train_essays.iloc[i]["essay"].split()
         unique_words = len(Counter(splits))
-        percentage = float(unique_words) / total_words
-        unique_word_percentages_train.append(percentage)
+        unique_word_percentages_train.append(unique_words)
 
-    for i in range(len(valid_df)):
-        splits = valid_df.iloc[i]["essay"].split()
-        total_words = len(splits)
+    for i in range(len(valid_essays)):
+        splits = valid_essays.iloc[i]["essay"].split()
         unique_words = len(Counter(splits))
-        percentage = float(unique_words) / total_words
-        unique_word_percentages_valid.append(percentage)
+        unique_word_percentages_valid.append(unique_words)
 
     #Add the features to the dataset
     train_df["unique_words"] = unique_word_percentages_train
